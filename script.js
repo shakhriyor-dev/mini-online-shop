727eacb9f2f32522fc38ffddf3430935ad5ca6eb
Shakhriyor <shakh2616@gmail.com>
2026-03-06 08:56:11 +0500

    script codes upgreted

--- script.js
+++ script.js
@@ -1,42 +1,130 @@
+// Product list
 let products = [
-{ name: "Laptop", price: 800 },
-{ name: "Phone", price: 500 },
-{ name: "Headphones", price: 100 }
+{
+name: "Laptop",
+price: 800,
+image: "images/laptop.jpg"
+},
+{
+name: "Phone",
+price: 500,
+image: "images/phone.jpg"
+},
+{
+name: "Headphones",
+price: 100,
+image: "images/headphones.jpg"
+}
 ];
 
+// Cart
 let cart = [];
-let total = 0;
 
+// Productlarni chiqarish
 let productContainer = document.getElementById("products");
 
-products.forEach((product, index) => {
+products.forEach((product,index)=>{
 
 productContainer.innerHTML += `
-<div class="col-md-4">
+<div class="col-md-4 mb-4">
 <div class="card p-3">
-<h4>${product.name}</h4>
+
+<img src="${product.image}" class="card-img-top">
+
+<h4 class="mt-2">${product.name}</h4>
+
 <p>$${product.price}</p>
+
 <button onclick="addToCart(${index})"
 class="btn btn-primary">
 Add to cart
 </button>
+
 </div>
 </div>
 `;
 
 });
 
+// Add to cart
 function addToCart(index){
 
 let product = products[index];
 
-cart.push(product);
+let existing = cart.find(item => item.name === product.name);
 
-document.getElementById("cart").innerHTML +=
-`<li>${product.name} - $${product.price}</li>`;
+if(existing){
 
-total += product.price;
+existing.quantity++;
+
+}else{
+
+cart.push({
+...product,
+quantity:1
+});
+
+}
+
+renderCart();
+
+}
+
+// Cartni chiqarish
+function renderCart(){
+
+let cartList = document.getElementById("cart");
+
+cartList.innerHTML = "";
+
+let total = 0;
+
+cart.forEach((item,index)=>{
+
+cartList.innerHTML += `
+<li class="mb-2">
+
+${item.name} - $${item.price}
+
+<button onclick="decrease(${index})"
+class="btn btn-sm btn-danger ms-2">-</button>
+
+<span class="mx-2">${item.quantity}</span>
+
+<button onclick="increase(${index})"
+class="btn btn-sm btn-success">+</button>
+
+</li>
+`;
+
+total += item.price * item.quantity;
+
+});
 
 document.getElementById("total").innerText = total;
+
+}
+
+// Quantity oshirish
+function increase(index){
+
+cart[index].quantity++;
+
+renderCart();
+
+}
+
+// Quantity kamaytirish
+function decrease(index){
+
+cart[index].quantity--;
+
+if(cart[index].quantity <= 0){
+
+cart.splice(index,1);
+
+}
+
+renderCart();
 
 }
